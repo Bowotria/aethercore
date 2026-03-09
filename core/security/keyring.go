ceb59f739ef085c5d00a9ecf10abc583556c0c47
fzihak <foysalmzihak@gmail.com>
2026-03-10 02:05:34 +0600

    feat(security): implement ParseEd25519PublicKey native Go logic

--- core/security/keyring.go
+++ core/security/keyring.go
@@ -1,8 +1,33 @@
 package security
 
-import "crypto/ed25519"
+import (
+	"crypto/ed25519"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+)
 
 type KeyRing struct {
 	trustedKeys []ed25519.PublicKey
 }
 func NewKeyRing() *KeyRing { return &KeyRing{} }
+
+func (k *KeyRing) LoadPEM(pemData []byte) error {
+	block, _ := pem.Decode(pemData)
+	if block == nil {
+		return errors.New("failed to decode PEM block containing public key")
+	}
+
+	pub, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return err
+	}
+
+	ed25519Key, ok := pub.(ed25519.PublicKey)
+	if !ok {
+		return errors.New("not an Ed25519 public key")
+	}
+
+	k.trustedKeys = append(k.trustedKeys, ed25519Key)
+	return nil
+}
